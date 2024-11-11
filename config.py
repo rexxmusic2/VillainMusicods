1ce39ed81537b10cd238575965d855dcda463cfb
VILLAIN <iamrkay420@gmail.com>
2024-11-12 01:08:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -90,10 +90,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/0d0d6323a7f0ea9f4ffcb.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/wrj4sw.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/51cb8a22e65caa4382879.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/wrj4sw.jpg"
 )
 PLAYLIST_IMG_URL = "https://telegra.ph/file/9acd828ec45a363add2e9.jpg"
 STATS_IMG_URL = "https://telegra.ph/file/51cb8a22e65caa4382879.jpg"
