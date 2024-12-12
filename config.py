ba6054884327eacdc1976bfaeb06403bbafa51f3
VILLAIN <iamrkay420@gmail.com>
2024-12-12 11:57:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -97,14 +97,14 @@ PING_IMG_URL = getenv(
 )
 PLAYLIST_IMG_URL = "https://files.catbox.moe/fcawaj.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/fcawaj.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/51cb8a22e65caa4382879.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/51cb8a22e65caa4382879.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/fcawaj.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/fcawaj.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/fcawaj.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/fcawaj.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/fcawaj.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/fcawaj.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/fcawaj.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/fcawaj.jpg"
 
 
 def time_to_seconds(time):
