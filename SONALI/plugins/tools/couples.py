54b0b94e746dad6cee55c6c4f848a7632cef8d14
rexxmusic <Tonyystark4412@gmail.com>
2024-12-28 17:40:56 +0530

    Update couples.py

--- SONALI/plugins/tools/couples.py
+++ SONALI/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from SONALI.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="ꪜ 𝛊 ɭ ɭ ᧘ 𝛊 𝛈",
-            url=f"https://t.me/iamakki001",
+            text="Ʀ Ξ 𝐗 𝐗",
+            url=f"https://t.me/REXX_DADDY",
         ),
     ],
 ]
