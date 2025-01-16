5cce3809d6f1740f3277b5981b847130486e7697
rexxmusic2 <rexxmusic8@gmail.com>
2025-01-16 19:20:52 +0530

    Update Gpt.py

--- SONALI/plugins/tools/Gpt.py
+++ SONALI/plugins/tools/Gpt.py
@@ -1,31 +1,30 @@
-import os
-import random
-import time
-from SONALI import app
-import requests
-from pyrogram.types import  Message
-from pyrogram.types import InputMediaPhoto
-from PurviAPI import api
-from pyrogram.enums import ChatAction, ParseMode
+from config import BANNED_USERS
 from pyrogram import filters
+from pyrogram.enums import ChatAction
+from PurviAPI import api
+from SONALI import app
 
 
-@app.on_message(
-    filters.command(
-        ["chatgpt", "ai", "ask", "gpt", "solve"],
-        prefixes=["+", ".", "/", "-", "", "$", "#", "&"],
-    )
-)
-async def chat_gpt(bot, message):
-    
-    try:
-        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
-        if len(message.command) < 2:
-            await message.reply_text(
-            "Example:**\n\n/chatgpt Where is golden temple?")
-        else:
-            a = message.text.split(' ', 1)[1]
-            r=api.gemini(a)["results"]
-            await message.reply_text(f" {r} \n\n🌸 ᴘᴏᴡᴇʀᴇᴅ ʙʏ @iamakki001", parse_mode=ParseMode.MARKDOWN)     
-    except Exception as e:
-        await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")
+@app.on_message(filters.command(["chatgpt", "ai", "ask"]) & ~BANNED_USERS)
+async def chatgpt_chat(bot, message):
+    if len(message.command) < 2 and not message.reply_to_message:
+        await message.reply_text(
+            "Example:\n\n`/ai write simple website code using html css, js?`"
+        )
+        return
+
+    if message.reply_to_message and message.reply_to_message.text:
+        user_input = message.reply_to_message.text
+    else:
+        user_input = " ".join(message.command[1:])
+
+    await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+    results = await api.chatgpt(user_input)
+    await message.reply_text(results)
+
+
+__MODULE__ = "CʜᴀᴛGᴘᴛ"
+__HELP__ = """
+/advice - ɢᴇᴛ ʀᴀɴᴅᴏᴍ ᴀᴅᴠɪᴄᴇ ʙʏ ʙᴏᴛ
+/ai [ǫᴜᴇʀʏ] - ᴀsᴋ ʏᴏᴜʀ ǫᴜᴇsᴛɪᴏɴ ᴡɪᴛʜ ᴄʜᴀᴛɢᴘᴛ's ᴀɪ
+/gemini [ǫᴜᴇʀʏ] - ᴀsᴋ ʏᴏᴜʀ ǫᴜᴇsᴛɪᴏɴ ᴡɪᴛʜ ɢᴏᴏɢʟᴇ's ɢᴇᴍɪɴɪ ᴀɪ"""
